232eac54ee0b1f7e106e0844d23412cdc923e61c
dawood@123 <dawoodeshraff123@gmail.com>
2025-04-03 22:09:11 +0530

    headerStyling done

--- Claude-chef/src/components/header.jsx
+++ Claude-chef/src/components/header.jsx
@@ -3,7 +3,7 @@ import chef from "./chef.png"
 function Header(){
     return(
         <div className="chef-main">
-            <img src={chef} alt="chef-icon" />
+            <img src={chef} alt="chef-icon" className="chef-image" />
       <p className="chef">Chef Claude</p>
     </div>
     )
